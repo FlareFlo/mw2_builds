947cda4a877c06890058bbf283974d50322534e6
FlareFlo <mail@flareflo.dev>
2022-12-05 14:13:13 +0100

    Add display for unlocking and weapon

--- src/unlock/unlock_condition.rs
+++ src/unlock/unlock_condition.rs
@@ -1,8 +1,30 @@
+use std::fmt::{Display, Formatter};
 use crate::weapon::Weapon;
 
 pub enum UnlockType {
 	PlayerLevel(usize),
-	Weapon(Weapon),
+	Weapon(Weapon, usize),
 	Custom(String),
 	Unknown,
+}
+
+impl Display for UnlockType {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{}",
+			match self {
+				UnlockType::PlayerLevel(level) => {
+					format!("Your player level needs to be a minimum of {level}")
+				}
+				UnlockType::Weapon(weapon, level) => {
+					format!("You have to progress {} to level {}", weapon.to_string(), level)
+				}
+				UnlockType::Custom(custom_text) => {
+					custom_text.clone()
+				}
+				UnlockType::Unknown => {
+					"Unknown unlock requirement".to_owned()
+				}
+			}
+		)
+	}
 }
--- src/weapon.rs
+++ src/weapon.rs
@@ -1,6 +1,14 @@
+use std::fmt::{Display, Formatter};
 use crate::build::WeaponClass;
 
 pub struct Weapon {
 	name: String,
 	class: WeaponClass,
+}
+
+// Placeholder for proper weapon rendering
+impl Display for Weapon {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{}", self.name)
+	}
 }
